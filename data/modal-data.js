b28a637555486e12f013df3609ad4175b0168d34
CMDChelp <greg.philbrook@wsu.edu>
2023-08-10 13:54:55 -0700

    Modal Update

--- data/modal-data.js
+++ data/modal-data.js
@@ -2,31 +2,37 @@
 const modalData = [
 	[`PATHWAY`,
 		`<header>
-			<h3 class="title">
+			<h2 class="title">
 				PATHWAY
-			</h3>
-			<h4 class="subTitle">A literary sequence</h4>
-			<h5 class="subTitle">providing a brief tour of the Writings</h5>
+			</h2>
+			<h3 class="subTitle">A literary sequence</h3>
+			<h4 class="subTitle">providing a brief tour of the Writings</h4>
 		</header>
-		
+
 		<div class="catalog container">
 			<p><a id="Epigraph"></a><a href="1Epigraph.html"><strong>Epigraph</strong></a></p>
 			<p><a id="Stormsandhaze"></a><a href="1Stormsandhaze.html">Storms and haze</a></p>
 			<p><a id="ScholarsDiary"></a><a href="1ScholarsDiary.html">Scholar's Diary</a></p>
 			<p><a id="TheLocustGrove"></a><a href="1TheLocustGrove.html">The Locust Grove</a></p>
-			<p><a id="EgderusatMountainHouse"></a><a href="1EgderusatMountainHouse.html">Egderus at Mountain House</p>
-			<p><a id="nocertaintiesnotruth"></a><a href="1nocertaintiesnotruth.html">no certainties, no truth</a></p>
+			<p><a id="EgderusatMountainHouse"></a><a href="1EgderusatMountainHouse.html">Egderus at Mountain House
+			</p>
+			<p><a id="nocertaintiesnotruth"></a><a href="1nocertaintiesnotruth.html">no certainties, no truth</a>
+			</p>
 			<p><a id="archiveunderthreat"></a><a href="1archiveunderthreat.html">archive under threat</a></p>
-			<p><a id="adreamoftheGreatCity"></a><a href="1adreamoftheGreatCity.html">a dream of the Great City</a></p>
-			<p><a id="SteadfastBone-Snapper"></a><a href="1SteadfastBone-Snapper.html">Steadfast Bone-Snapper</a></p>
+			<p><a id="adreamoftheGreatCity"></a><a href="1adreamoftheGreatCity.html">a dream of the Great City</a>
+			</p>
+			<p><a id="SteadfastBone-Snapper"></a><a href="1SteadfastBone-Snapper.html">Steadfast Bone-Snapper</a>
+			</p>
 			<p><a id="intheKeep"></a><a href="1intheKeep.html">in the Keep</a></p>
 			<p><a id="ThePerpetratedWorld"></a><a href="1ThePerpetratedWorld.html">The Perpetrated World</a></p>
-			<p><a id="Thescriptorandhisbeloved"></a><a href="1Thescriptorandhisbeloved.html">The scriptor and his beloved</a></p>
+			<p><a id="Thescriptorandhisbeloved"></a><a href="1Thescriptorandhisbeloved.html">The scriptor and his
+					beloved</a></p>
 			<p><a id="stars"></a><a href="1stars.html">stars</a></p>
 			<p><a id="enemy"></a><a href="1enemy.html">enemy</a></p>
 			<p><a id="return"></a><a href="1return.html">return</a></p>
 			<p><a id="thisroad"></a><a href="1thisroad.html">this road</a></p>
-			<p><a id="LANDING"></a><a href="LANDING.html"><strong>LANDING</strong></a><p>
+			<p><a id="LANDING"></a><a href="LANDING.html"><strong>LANDING</strong></a>
+			<p>
 		</div>
 		<!-- end of "catalog" div -->`
 	],
